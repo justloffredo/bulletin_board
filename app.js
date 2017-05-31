b7da2f856977e5642837a7af45cbfabde9be21fa
Justin Loffredo <justloffredo@gmail.com>
2017-05-31 18:26:57 -0400

    bulletin initial commit

---
+++ app.js
@@ -0,0 +1,39 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const bodyParser = require('body-Parser');
+const bulletinBoardRoutes = require('./Routes/bulletin_board.js');
+const query = require('./utility/query.js');
+
+
+app.set('view-engine', 'ejs');
+app.use(express.static('assets'));
+
+
+app.use(bodyParser.urlencoded());
+app.use(bodyParser.json());
+
+app.get("/list", function(req, res) {
+	query("SELECT * FROM messages")
+	.then(function(data) {
+		res.json(data);
+	});
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	app.listen(3000, function() {
+		console.log("Your server is available at 3000");
+	});
